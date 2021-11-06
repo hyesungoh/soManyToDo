dd0e8bc27a848ed94b21f1f435b8829bcbfb7d30
Hyesung Oh <haesungoh414@gmail.com>
2021-11-07 03:05:19 +0900

    feat: apply api, config js at vanilla

--- vanilla/index.js
+++ vanilla/index.js
@@ -1,6 +1,20 @@
+// import axios from "axios";
+import { get, post, del } from "./api";
+
 const list = document.querySelector(".js-list");
 const form = document.querySelector("form");
 
-function init(){
-    
+async function getToDos() {
+    const response = await get("/todo?maxRecords=3&view=Grid%20view");
+    console.log(response);
 }
+
+function generateToDos() {
+    getToDos();
+}
+
+function init() {
+    generateToDos();
+}
+
+init();
