d97480e85040982d71dbc29e1c0c882ea7964c7f
Hyesung Oh <haesungoh414@gmail.com>
2021-11-12 00:20:31 +0900

    feat: apply delete todo at react

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -3,12 +3,12 @@ import TodoList from "./components/TodoList";
 import useTodos from "./hooks/useTodos";
 
 function App() {
-  const { todos, createTodo } = useTodos();
+  const { todos, createTodo, deleteTodo } = useTodos();
 
   return (
     <div>
       <h1>oh to do react</h1>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
       <TodoForm createTodo={createTodo} />
     </div>
   );
--- react/src/components/TodoList.tsx
+++ react/src/components/TodoList.tsx
@@ -1,14 +1,26 @@
+import { MouseEvent } from "react";
 import { ITodo } from "../hooks/useTodos";
 
 interface TodoListProps {
   todos: ITodo[];
+  deleteTodo: (todoId: string) => void;
 }
 
-function TodoList({ todos }: TodoListProps) {
+function TodoList({ todos, deleteTodo }: TodoListProps) {
+  const onClickDelete = (e: MouseEvent<HTMLButtonElement>) => {
+    const { parentNode } = e.currentTarget;
+    const { id } = parentNode as HTMLElement;
+    deleteTodo(id);
+  };
+
   return (
     <ul>
       {todos.map((todo) => (
-        <li>{todo.fields.name}</li>
+        <li id={todo.id}>
+          {todo.fields.name}
+
+          <button onClick={onClickDelete}>X</button>
+        </li>
       ))}
     </ul>
   );
