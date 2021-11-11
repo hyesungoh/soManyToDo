d32c0653a86c47f7b72fd663be194998068ab106
Hyesung Oh <haesungoh414@gmail.com>
2021-11-11 23:29:38 +0900

    feat: apply response interceptor at react

--- react/src/lib/api.tsx
+++ react/src/lib/api.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 
 const instance = axios.create({
   baseURL: "https://api.airtable.com/v0/app5AO0SumKR9cNKp",
@@ -12,6 +12,30 @@ const requestInterceptor = (config: AxiosRequestConfig) => ({
 });
 
 instance.interceptors.request.use(requestInterceptor);
+
+const responseFulfilledInterceptor = (res: AxiosResponse) => {
+  if (res.status >= 200 && res.status < 300) {
+    return res.data;
+  }
+
+  return Promise.reject(res.data);
+};
+
+const responseRejectedInterceptor = (error: AxiosError) => {
+  if (error.response?.data.message != null) {
+    return {
+      ...error.response.data,
+      message: error.response.data.message,
+    };
+  }
+
+  return new Error(error.response?.data.message ?? error);
+};
+
+instance.interceptors.response.use(
+  responseFulfilledInterceptor,
+  responseRejectedInterceptor
+);
 
 export const get = <T,>(...args: Parameters<typeof instance.get>) =>
   instance.get<T, T>(...args);
